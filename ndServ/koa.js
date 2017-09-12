af845441f03b8f222c5402c135ee368da68e4ef6
jkerush <zzh3258@qq.com>
2017-09-12 11:38:28 +0800

    learn koa2

--- ndServ/koa.js
+++ ndServ/koa.js
@@ -1,9 +1,17 @@
 const Koa = require('koa')
+const fs = require('fs')
+const route = require('koa-route')
 const app = new Koa()
 
-app.use(async(ctx) => {
-    ctx.body = "Hello koa2"
-})
+const main = ctx => {
+    ctx.response.body = "hello koa.js! " + new Date().toLocaleString()
+}
+const home = ctx => {
+    ctx.response.type = 'html'
+    ctx.response.body = fs.createReadStream(__dirname + "/static/view/index.html")
+}
+app.use(route.get("/home", home))
+app.use(route.get("/", main))
 
 app.listen(3000)
 
