cb8a16140297346427f8dcafd49e11d19bbaac9c
jkerush <zzh3258@qq.com>
2017-09-18 16:18:20 +0800

    add saolei

--- ndServ/koa.js
+++ ndServ/koa.js
@@ -1,18 +1,46 @@
 const Koa = require('koa')
 const fs = require('fs')
-const route = require('koa-route')
+const Router = require('koa-router')
 const app = new Koa()
 
-const main = ctx => {
-    ctx.response.body = "hello koa.js! " + new Date().toLocaleString()
-}
-const home = ctx => {
-    ctx.response.type = 'html'
-    ctx.response.body = fs.createReadStream(__dirname + "/static/view/index.html")
-}
-app.use(route.get("/home", home))
-app.use(route.get("/", main))
 
-app.listen(3000)
+const mongoose = require('mongoose')
+const User = require('./model/users')
 
-console.log('now start-quick is starting at port 3000')
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/webs', { useMongoClient: true, })
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'))
+    .once('openUri', function() {
+        console.log("we're connected!")
+    });
+
+
+
+// 获取数据 
+User.find({ name: 'jack' }).exec((err, person) => {
+    if (err) {
+        console.log(err)
+    } else {
+        // console.log(person)
+    }
+})
+
+
+//路由
+let router = new Router()
+router
+    .get("/", (ctx, next) => {
+        ctx.response.body = "Landing"
+    })
+    .get("/index", async(ctx, next) => {
+        ctx.response.type = 'html'
+        ctx.response.body = await fs.createReadStream(__dirname + "/static/view/index.html")
+    })
+
+app.use(router.routes()).use(router.allowedMethods())
+
+// 监听3000
+app.listen(3000, () => {
+    console.log('now start-quick is starting at port 3000')
+})
